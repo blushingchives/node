884617956aa79db3fc5d4de6a20b1f952412d165
blushingchives <52131462+blushingchives@users.noreply.github.com>
2023-07-07 17:48:36 +0800

    update script

--- rpc/create_docker_container.sh
+++ rpc/create_docker_container.sh
@@ -43,4 +43,26 @@ if [[ -z $(docker image ls | grep "${daemon_name}") ]]; then
     exit
 fi
 
-docker stop ${container_name} && docker rm ${container_name} || true && docker run -d --volume ${container_name}-volume:${node_home} --name ${container_name} kujirad bash -c "$(cat run.sh)"
+docker stop ${container_name} && \
+docker rm ${container_name} || true && \
+docker run \
+    -it \
+    --volume ${container_name}-volume:${node_home} \
+    --name ${container_name} \
+    kujirad \
+    bash -c \
+    " \
+    echo \"$(cat ../chains/${chain_name}/config/app.toml | sed 's/\"/<>!spacing<>/g')\" > /root/.kujira/config/app.toml && \
+    sed -i 's/<>!spacing<>/\"/g' /root/.kujira/config/app.toml && \
+    echo \"$(cat ../chains/${chain_name}/config/config.toml | sed 's/\"/<>!spacing<>/g')\" > /root/.kujira/config/config.toml && \
+    sed -i 's/<>!spacing<>/\"/g' /root/.kujira/config/config.toml && \
+    $(cat run.sh) \
+    "
+        # ls /root/.kujira/config \
+    # $(cat run.sh) \
+
+    #     """ \
+    # echo \"$(cat ../chains/${chain_name}/config/config.toml | sed 's/\"/<>!spacing<>/g')\" > /root/.kujira/config/config.toml | sed 's/| sed 's/\"/<>!spacing<>/g'/\"/g' && \
+    # echo \"$(cat ../chains/${chain_name}/config/app.toml | sed 's/\"/<>!spacing<>/g')\" > /root/.kujira/config/app.toml | sed 's/| sed 's/\"/<>!spacing<>/g'/\"/g' &&  \
+    # cat /root/.kujira/config/app.toml \
+    # """
--- rpc/run.sh
+++ rpc/run.sh
@@ -4,9 +4,9 @@ kujirad init node --chain-id kaiyo-1 --home /root/.kujira
 SEEDS=ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@seeds.polkachu.com:11856,63158c2af0d639d8105a8e6ca2c53dc243dd156f@seed.kujira.mintserve.org:31897
 sed -i.bak -e "s/^seeds *=.*/seeds = \"$SEEDS\"/" /root/.kujira/config/config.toml
 
-SNAP_RPC="https://kujira-rpc.polkachu.com:443"
+SNAP_RPC="https://kujira.rpc.kjnodes.com:443"
 LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000)); \
 TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
 sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
 s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
