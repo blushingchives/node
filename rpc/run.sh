2423446cc995c60180794d9c7473ab37c539e802
blushingchives <52131462+blushingchives@users.noreply.github.com>
2023-12-07 13:17:46 +0800

    update

--- rpc/run.sh
+++ rpc/run.sh
@@ -5,7 +5,7 @@ curl -s -H 'Cache-Control: no-cache' -o /root/.kujira/config/config.toml https:/
 curl -s -H 'Cache-Control: no-cache' -o /root/.kujira/config/app.toml https://raw.githubusercontent.com/blushingchives/node/main/chains/kujira/config/app.toml
 curl -s -H 'Cache-Control: no-cache' -o /etc/nginx/nginx.conf https://raw.githubusercontent.com/blushingchives/node/main/rpc/nginx.conf
 
-SEEDS=ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@seeds.polkachu.com:11856,63158c2af0d639d8105a8e6ca2c53dc243dd156f@seed.kujira.mintserve.org:31897
+SEEDS=ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@seeds.polkachu.com:11856,63158c2af0d639d8105a8e6ca2c53dc243dd156f@seed.kujira.mintserve.org:31897,20e1000e88125698264454a884812746c2eb4807@seeds.lavenderfive.com:11856,824fa337b806bd48ce9505d74ba3e5adea80da93@seeds.goldenratiostaking.net:1628,322abfd7c0bcdf8a3d98ccb46ae2572bae0e8301@seed-kujira.starsquid.io:15602,c28827cb96c14c905b127b92065a3fb4cd77d7f6@seeds.whispernode.com:11856
 sed -i.bak -e "s/^seeds *=.*/seeds = \"$SEEDS\"/" /root/.kujira/config/config.toml
 
 SNAP_RPC="https://kujira-rpc.polkachu.com:443"
