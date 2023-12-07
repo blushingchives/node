5d82cd4fd0ea25818c0892ddb05628d6b75d78bf
blushingchives <52131462+blushingchives@users.noreply.github.com>
2023-12-07 16:02:02 +0800

    add peer

--- rpc/run.sh
+++ rpc/run.sh
@@ -5,7 +5,7 @@ curl -s -H 'Cache-Control: no-cache' -o /root/.kujira/config/config.toml https:/
 curl -s -H 'Cache-Control: no-cache' -o /root/.kujira/config/app.toml https://raw.githubusercontent.com/blushingchives/node/main/chains/kujira/config/app.toml
 curl -s -H 'Cache-Control: no-cache' -o /etc/nginx/nginx.conf https://raw.githubusercontent.com/blushingchives/node/main/rpc/nginx.conf
 
-PEERS=030f65339defb01b0e3ddaeaa54cbeac00dd0c74@185.182.193.89:26656,c030a6692eb4f39b00b1dbb68e47698230bc2de9@142.132.248.34:11856,70bb20d6078ff90294ebd7c9803de25b73d48955@148.251.77.27:26656,338d79e24ce36a9580ce3e9fce8eeb84e0e6f17b@65.108.130.171:26656,9158c675c4337b5010a3f65857d68c2a3326c2e2@107.181.234.234:26656
+PEERS=9b7310c0510b26794a204912de0a9c88c19e5b85@65.108.204.225:11856,030f65339defb01b0e3ddaeaa54cbeac00dd0c74@185.182.193.89:26656,c030a6692eb4f39b00b1dbb68e47698230bc2de9@142.132.248.34:11856,70bb20d6078ff90294ebd7c9803de25b73d48955@148.251.77.27:26656,338d79e24ce36a9580ce3e9fce8eeb84e0e6f17b@65.108.130.171:26656,9158c675c4337b5010a3f65857d68c2a3326c2e2@107.181.234.234:26656
 sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" /root/.kujira/config/config.toml
 
 SNAP_RPC="https://kujira-rpc.polkachu.com:443"
